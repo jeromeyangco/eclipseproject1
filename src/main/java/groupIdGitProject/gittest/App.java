99f8815a40cab541ab03eb046363ad7eda75ae66
jeromeyangco <Jerome Yangco@JeromeYangco>
2018-11-03 10:25:53 +0800

    git test

---
+++ src/main/java/groupIdGitProject/gittest/App.java
@@ -0,0 +1,13 @@
+package groupIdGitProject.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
